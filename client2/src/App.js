690d7b77443a2da26415f73caf51200583eeabce
KhornSokheng <59908402+KhornSokheng@users.noreply.github.com>
2021-10-08 00:06:14 +0700

    create router

--- client2/src/App.js
+++ client2/src/App.js
@@ -2,14 +2,55 @@ import './App.css';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Home from './components/Home';
+import Buy from './components/Buy';
+import Sale from './components/Sale';
+import Size from './components/Size';
+import Product from './components/Product';
+
 import InsertCustomer from './components/InsertCustomer';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Banner/>
-      <InsertCustomer/>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/customer">
+            <InsertCustomer/>
+          </Route>
+          <Route path="/customer/:id">
+            <InsertCustomer/>
+          </Route>
+          <Route path="/product">
+            <Product/>
+          </Route>
+          <Route path="/Buy">
+            <Buy/>
+          </Route>
+          <Route path="/Buy/:id">
+            <Buy/>
+          </Route>
+          <Route path="/Sale">
+            <Sale/>
+          </Route>
+          <Route path="/Size">
+            <Size/>
+          </Route>
+        </Switch>
+
+
+      </Router>
       <Footer/>
     </div>
   );
--- client2/src/components/InsertCustomer.js
+++ client2/src/components/InsertCustomer.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function Input() {
+export default function Customer() {
   const [cust_id, setID] = useState("C0000");
   const [cust_name, setName] = useState("Name");
   const [cust_lname, setLName] = useState("LastName");
@@ -9,14 +9,13 @@ export default function Input() {
     e.preventDefault();
     try {
       // console.log(name, price);
-      const bodyStr = { cust_id,cust_name,cust_lname };
+      const bodyStr = { cust_id, cust_name, cust_lname };
       const resp = fetch("http://localhost:5000/insertCustomer", {
         method: "POST",
         headers: { "content-type": "application/json" },
         body: JSON.stringify(bodyStr),
-      })
+      });
       window.location = "/";
-      
     } catch (err) {
       console.error(err.message);
     }
@@ -25,8 +24,7 @@ export default function Input() {
   return (
     <div className="container">
       <form onSubmit={doInsertForm}>
-
-      <div className="form-group">
+        <div className="form-group">
           <label htmlFor="name">Cust ID:</label>
           <input
             type="name"
@@ -38,7 +36,7 @@ export default function Input() {
             }}
           />
         </div>
-        
+
         <div className="form-group">
           <label htmlFor="name">Cust name:</label>
           <input
@@ -50,7 +48,6 @@ export default function Input() {
               setName(e.target.value);
             }}
           />
-          
         </div>
         <div className="form-group">
           <label htmlFor="name">Cust lastname:</label>
