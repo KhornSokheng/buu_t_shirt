b3ecdb88608f54f498d5467101997e769548ce41
phongsakornchantapoon <59908421+phongsakornchantapoon@users.noreply.github.com>
2021-10-09 16:01:31 +0700

    select buy

--- client2/src/components/Buy.js
+++ client2/src/components/Buy.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 export default function Buy() {
+    const [list,setList] = useState([]);
+    const loadList = async()=>{
+        try {
+            const resp = await fetch("http://localhost:3000/getBuy")
+            const jsonData = await resp.json();
+
+            setList(jsonData);
+
+            console.log("Resp",resp);
+            console.log("List:",list);
+
+        }   catch (err) {
+            console.error(err.message);
+        }
+    }
+      useEffect(()=>{
+          console.log("Enter useEffect()");
+          loadList();
+      })
     return (
-        <div>
-            <h1>Buy</h1>
+        <div className="container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Buy date</th>
+                        <th>Buy id</th>
+                        <th>Buy status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        {
+                            list.map(elt=>{
+                                return(
+                                    <tr>
+                                    <td>{elt.buy_date}</td>
+                                    <td>{elt.buy_id}</td>
+                                    <td>{elt.buy_status}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
