71cf00b25909bdf70e7e722df649fa1af88d19a8
janejira12 <62310094@go.buu.ac.th>
2021-10-14 15:58:33 +0700

    login

--- client2/src/components/LogIn.js
+++ client2/src/components/LogIn.js
@@ -1,96 +1,102 @@
-import {React,useState} from 'react'
-import {Form,Row,InputGroup,Button,Col} from "react-bootstrap";
+import { React, useState } from "react";
+import { Form, Row, InputGroup, Button, Col } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 export default function LogIn() {
-    
+  const [validated, setValidated] = useState(false);
 
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
+    setValidated(true);
+  };
 
-    const [validated, setValidated] = useState(false);
-  
-    const handleSubmit = (event) => {
-      const form = event.currentTarget;
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-  
-      setValidated(true);
-    };
-  
-    return (
-        <Form className="container align-items-center" noValidate validated={validated} onSubmit={handleSubmit}>
-        <Row className="mb-3">
-          {/* <Form.Group as={Col} md="4" controlId="validationCustom01">
-            <Form.Label>First name</Form.Label>
-            <Form.Control
-              required
-              type="text"
-              placeholder="First name"
-              defaultValue="Mark"
-            />
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          </Form.Group> */}
-          <Form.Group as={Col} md="4" controlId="validationCustom02">
-            <Form.Label>Last name</Form.Label>
-            <Form.Control
-              required
-              type="text"
-              placeholder="Last name"
-              defaultValue="Otto"
-            />
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-            <Form.Label>Username</Form.Label>
-            <InputGroup hasValidation>
-              <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+  return (
+    <div className="singin container">
+      <Card border="success">
+        <Card.Header
+          className="d-flex justify-content-center alert alert-success"
+          role="alert"
+        >
+          <h3>SIGN IN</h3>
+        </Card.Header>
+        <Form
+          className="container align-items-center"
+          noValidate
+          validated={validated}
+          onSubmit={handleSubmit}
+        >
+          <Row className="mb-3">
+            <Form.Group as={Col} md="6" controlId="validationCustom01">
+              <Form.Label>First name</Form.Label>
+              <Form.Control required type="text" placeholder="First name" />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="6" controlId="validationCustom02">
+              <Form.Label>Last name</Form.Label>
               <Form.Control
+                required
                 type="text"
-                placeholder="Username"
-                aria-describedby="inputGroupPrepend"
+                placeholder="Last name"
                 required
               />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="12" controlId="validationCustomEmail">
+              <Form.Label>Email address</Form.Label>
+              <InputGroup hasValidation>
+                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  placeholder="name@example.com"
+                  aria-describedby="inputGroupPrepend"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please choose a Email.
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="6" controlId="validationCustom03">
+              <Form.Label>Phone number</Form.Label>
+              <Form.Control type="text" placeholder="Phone number" required />
               <Form.Control.Feedback type="invalid">
-                Please choose a username.
+                Please provide a valid Phone number.
               </Form.Control.Feedback>
-            </InputGroup>
+            </Form.Group>
+            <Form.Group as={Col} md="6" controlId="validationCustom04">
+              <Form.Label>Credit card</Form.Label>
+              <Form.Control type="text" placeholder="Credit card" required />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid Credit card.
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="12" controlId="validationCustom05">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" required />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid Password.
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Form.Group className="mb-3">
+            <Form.Check
+              required
+              label="Agree to terms and conditions"
+              feedback="You must agree before submitting."
+              feedbackType="invalid"
+            />
           </Form.Group>
-        </Row>
-        <Row className="mb-3">
-          {/* <Form.Group as={Col} md="6" controlId="validationCustom03">
-            <Form.Label>City</Form.Label>
-            <Form.Control type="text" placeholder="City" required />
-            <Form.Control.Feedback type="invalid">
-              Please provide a valid city.
-            </Form.Control.Feedback>
-          </Form.Group> */}
-          <Form.Group as={Col} md="3" controlId="validationCustom04">
-            <Form.Label>State</Form.Label>
-            <Form.Control type="text" placeholder="State" required />
-            <Form.Control.Feedback type="invalid">
-              Please provide a valid state.
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group as={Col} md="3" controlId="validationCustom05">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control type="text" placeholder="Zip" required />
-            <Form.Control.Feedback type="invalid">
-              Please provide a valid zip.
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Row>
-        <Form.Group className="mb-3">
-          <Form.Check
-            required
-            label="Agree to terms and conditions"
-            feedback="You must agree before submitting."
-            feedbackType="invalid"
-          />
-        </Form.Group>
-        <Button type="submit">Submit form</Button>
-      </Form>
-    )
+          <Button type="submit">Submit form</Button>{" "}
+        </Form>
+      </Card>
+    </div>
+  );
 }
