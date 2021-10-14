1e59baa8a83f667def6147db1170bca4418647e6
janejira12 <62310094@go.buu.ac.th>
2021-10-14 18:09:01 +0700

    add singup

--- client2/src/components/LogIn.js
+++ client2/src/components/LogIn.js
@@ -1,6 +1,7 @@
 import { React, useState } from "react";
 import { Form, Row, InputGroup, Button, Col } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function LogIn() {
@@ -18,12 +19,12 @@ export default function LogIn() {
 
   return (
     <div className="singin container">
-      <Card border="success">
+      <Card border="info">
         <Card.Header
-          className="d-flex justify-content-center alert alert-success"
+          className="d-flex justify-content-center alert alert-info"
           role="alert"
         >
-          <h3>SIGN IN</h3>
+          <h3>LOGIN</h3>
         </Card.Header>
         <Form
           className="container align-items-center"
@@ -32,21 +33,6 @@ export default function LogIn() {
           onSubmit={handleSubmit}
         >
           <Row className="mb-3">
-            <Form.Group as={Col} md="6" controlId="validationCustom01">
-              <Form.Label>First name</Form.Label>
-              <Form.Control required type="text" placeholder="First name" />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="6" controlId="validationCustom02">
-              <Form.Label>Last name</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="Last name"
-                required
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
             <Form.Group as={Col} md="12" controlId="validationCustomEmail">
               <Form.Label>Email address</Form.Label>
               <InputGroup hasValidation>
@@ -64,20 +50,6 @@ export default function LogIn() {
             </Form.Group>
           </Row>
           <Row className="mb-3">
-            <Form.Group as={Col} md="6" controlId="validationCustom03">
-              <Form.Label>Phone number</Form.Label>
-              <Form.Control type="text" placeholder="Phone number" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid Phone number.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="6" controlId="validationCustom04">
-              <Form.Label>Credit card</Form.Label>
-              <Form.Control type="text" placeholder="Credit card" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid Credit card.
-              </Form.Control.Feedback>
-            </Form.Group>
             <Form.Group as={Col} md="12" controlId="validationCustom05">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" required />
@@ -86,15 +58,13 @@ export default function LogIn() {
               </Form.Control.Feedback>
             </Form.Group>
           </Row>
-          <Form.Group className="mb-3">
-            <Form.Check
-              required
-              label="Agree to terms and conditions"
-              feedback="You must agree before submitting."
-              feedbackType="invalid"
-            />
-          </Form.Group>
-          <Button type="submit">Submit form</Button>{" "}
+
+          <Button type="submit">Submit form</Button>
+          <br></br>
+          <br></br>
+          <Card.Link href="/Signup">SING UP</Card.Link>
+          <br></br>
+          <br></br>
         </Form>
       </Card>
     </div>
